8f989a2ee3568e8b3830ce0bf7212f3325ffb0d7
Dima-Golovatuk1 <146225071+Dima-Golovatuk1@users.noreply.github.com>
2024-09-15 15:53:27 +0300

    1

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html',  products=products)
+    return render_template('index.html',  products=products_list)
 
 
 @app.route('/buy_product/<int:id>/')
