26d1cdc3ff478c20483ae54916ff50e582ef1ac5
Dima-Golovatuk1 <146225071+Dima-Golovatuk1@users.noreply.github.com>
2024-09-16 18:48:05 +0300

    updata

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from data import get_products, get__one__products
+from flask import Flask, render_template, request, redirect, url_for
+from data import get_products, get__one__products, put_feedback, get_feedback
 import random
 
 app = Flask(__name__)
@@ -11,19 +11,25 @@ def index():
     return render_template('index.html',  products=products_list)
 
 
-@app.route('/buy_product/<int:id>/')
-def tours(id):
-    print(id)
+@app.route('/buy_product/<int:id>/', methods=["GET", "POST"])
+def buy_product(id):
     products_list = get_products()
-    product = get__one__products(id-1)
-    num = random.randrange(1, len(products_list))
-    product_random = get__one__products(num)
-    
     if id <= len(products_list) and id > 0:
+        product = get__one__products(id - 1)
         num = random.randrange(1, len(products_list))
+        product_random = get__one__products(num)
+        feedback_list = get_feedback()
+        
+        if request.method == 'POST':
+            name = request.form['name']
+            feedback = request.form['feedback']
+            put_feedback(name, feedback, id)
+            return redirect(url_for('buy_product', id=id))
+        
         return render_template('buy_product.html',
-                                product = product,
-                                product_random = product_random)
+                                product=product,
+                                product_random=product_random,
+                                feedback_list=feedback_list)
     else:
         return "Product not found", 404
 
--- data.py
+++ data.py
@@ -22,6 +22,7 @@ def get_products():
             """SELECT * FROM products"""
         )
         products_list = cursor.fetchall()
+        print(products_list)
         return products_list
 
     except Exception as _ex:
@@ -52,7 +53,6 @@ def get__one__products(id):
             """SELECT * FROM products LIMIT 1 OFFSET %s""", (id,)
         )
         product = cursor.fetchall()
-        print(product)
         return product
 
     except Exception as _ex:
@@ -64,4 +64,64 @@ def get__one__products(id):
             print('[INFO] PostgreSQL connection closed')
 
 
-get__one__products(6)
+def put_feedback(name, feedback, id):
+    host = '127.0.0.1'
+    user = 'postgres'
+    password = '08112007'
+    db_name = 'HW-39shop'
+
+
+    try:
+        connection = psycopg2.connect(
+            host=host,
+            user=user,
+            password=password,
+            database = db_name
+        )
+
+        cursor = connection.cursor()
+        cursor.execute(
+            """INSERT INTO feedback (user_name, feedback, id_product) VALUES (%s, %s, %s)""",
+            (name, feedback, id)
+        )
+        connection.commit()
+
+    except Exception as _ex:
+        print('[INFO] Error while working with PostgreSQL', _ex)
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+            print('[INFO] PostgreSQL connection closed')
+
+
+def get_feedback():
+    host = '127.0.0.1'
+    user = 'postgres'
+    password = '08112007'
+    db_name = 'HW-39shop'
+
+
+    try:
+        connection = psycopg2.connect(
+            host=host,
+            user=user,
+            password=password,
+            database = db_name
+        )
+
+        cursor = connection.cursor()
+        cursor.execute(
+            """SELECT * FROM feedback"""
+        )
+        feedback_list = cursor.fetchall()
+        return feedback_list
+
+    except Exception as _ex:
+        print('[INFO] Error while working with PostgreSQL', _ex)
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+            print('[INFO] PostgreSQL connection closed')
+get_products()
